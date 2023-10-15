b540f2dc8a8536639d94a011c9f490a740c50752
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-10-15 09:21:31 +0530

    Adding New Files

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ echo "Peddireddy Hari Vardhan Reddy"
 echo "Ramesh : He is Working as a analyst"
 echo "Suresh is worksing with devops engineer"
 
-person1=Hari Vardhan
+person1=HariVardhan
 person2=Peddireddy
 
 echo "${person1} : He is Working as a analyst"
